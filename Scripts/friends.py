89a5d1607af90ea606363160d6b610e95cb193d2
Diksha65 <agarwaldiksha6596@gmail.com>
2019-04-02 20:48:34 +0530

    Finally working. But to be made more generic

--- Scripts/friends.py
+++ Scripts/friends.py
@@ -1,8 +1,8 @@
-import os, random, sys, vlc
+import os, random, sys, vlc, glob
 
 media_list = []
 srt_list=[]
-path = "/media/diksha/BUBBLES/Leisure_Time/series/Friends"
+path="/media/diksha/BUBBLES/Leisure_Time/series/Friends"
 
 def change_dir():
     os.chdir(path)
@@ -11,27 +11,26 @@ def change_dir():
     os.chdir(files[index])                                        #append this folder to the path
     return os.getcwd() 
 
-def create_media_list():
-    new_path = change_dir()                                       #get the currect path
+def create_media_list(new_path):
     for path, subdirs, files in os.walk(new_path):                  
         for file in files:                                              
-            if file.lower().endswith(('.mkv', '.avi')):
-                media_list.append(os.path.join(new_path, file))   #create a media list
+            if file.lower().endswith(('.mkv', '.avi', '.mp4')):
+                media_list.append(file)                          #create a media list
             if file.endswith(('.srt')):
-                srt_list.append(os.path.join(new_path, file))     #create a srt list
+                srt_list.append(file)                            #create a srt list
 
-def select_random_file():
-    create_media_list()                                                         
-    media = random.choice(media_list)                             #randomly select any media file
+def select_random_file(new_path):
+    create_media_list(new_path)                                                         
+    media = random.choice(media_list)                            #randomly select any media file
     srtfile = ""
     if len(srt_list) != 0:
-        media_episode = media.split('/')[8][:-4]                  #fetch the episode name
+        media_episode = media[:-4]                               #fetch the episode name
         for srt in srt_list:
-            srt_episode = srt.split('/')[8][:-4]                  #fetch the srt name
-            if srt_episode == media_episode:                      #if srt is for the corresponding media, then save
-                srtfile = srt 
+            srt_episode = srt[:-4]                               #fetch the srt name
+            if srt_episode == media_episode:                     #if srt is for the corresponding media, then save
+                srtfile = os.path.join(new_path, srt)
                 break
-    return (media, srtfile)
+    return ( os.path.join(new_path, media),srtfile)
 
 """ def play_episode():
     video, srt = select_random_file()
@@ -44,8 +43,8 @@ def select_random_file():
 """
 
 def main():
-    video, srt = select_random_file()
-    print(video, ',', srt)
+    video, srt = select_random_file(change_dir())
+    print(video,',', srt)
 
 if __name__ == "__main__":
     main() 
